07857ffc74823dddf567e337f1cf109c4bdb0fe6
dmangiardi <dominic.mangiardi@lifecanvastech.com>
2023-05-30 14:56:24 -0400

    23 done

--- experiments/BrainLine/scripts/soma_detect_image.py
+++ experiments/BrainLine/scripts/soma_detect_image.py
@@ -9,12 +9,12 @@ Inputs
 # -dir_base
 # data_dir and results_dir ARE CLEAR
 # threshold IS CORRECT
-brain = "MS36"
+brain = "MS23"
 antibody_layer = "Ch_647"
 background_layer = "Ch_561"
 endogenous_layer = "Ch_488"
 
-threshold = 0.54  # threshold to use for ilastik
+threshold = 0.58  # threshold to use for ilastik
 brainline_exp_dir = Path(os.getcwd()) / Path(__file__).parents[1]
 data_dir = (
     brainline_exp_dir / "data" / "brainr_temp"
@@ -33,15 +33,15 @@ ilastik_project = "/home/user/misc_tommy/ilastik/models/soma/matt_soma_rabies_pi
 
 min_coords = [
     -1,
-    -1,
+    1043,
     -1,
 ]  # max coords or -1 if you want to process everything along that dimension
 max_coords = [
     -1,
-    10000,
-    -1,
+    9210,
+    5941,
 ]  # max coords or -1 if you want to process everything along that dimension
-ncpu = 1  # 16  # number of cores to use for detection
+ncpu = 2  # 16  # number of cores to use for detection
 chunk_size = [512, 1024, 2560]  # [256, 256, 300]
 
 """ 
